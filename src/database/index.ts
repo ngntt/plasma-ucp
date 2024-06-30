36f54e797c83e24a3a877b51143450dca30ca2d2
ngntt <95158019+ngntt@users.noreply.github.com>
2024-06-30 12:48:44 +0700

    change databse

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql2";
 
 export const connection = mysql.createConnection({
-  host: "204.10.192.68",
-  user: "u479_rZxkCrcX3B",
-  password: ".w0MCO4myhjyAm=@!vAIGUUR",
-  database: "s479_plasmacity_samp",
+  host: "samp1.dopminer.host",
+  user: "samp_plasma_2",
+  password: "JD83!0ri1Ljo",
+  database: "samp_plasma_server"
 });
